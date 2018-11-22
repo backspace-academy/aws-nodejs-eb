e8476819688f77f456743fa3f197b5cffe07cad2
BackSpace Academy <31625808+backspace-academy@users.noreply.github.com>
2018-11-23 02:13:36 +1100

    .

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'BackSpace Academy Green Deployment'
+    title: 'BackSpace Academy & Elastic Beanstalk'
     })
     res.status(200).end();
 })
