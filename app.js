21645f1d05ebf239183a877986a8554377f62256
andylightfoot <andrew.lightfoot@digital.justice.gov.uk>
2021-08-24 15:20:07 +0100

    test change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'BackSpace Academy & Elastic Beanstalk'
+    title: 'Lightfoot Academy & Elastic Beanstalk'
     })
     res.status(200).end();
 })
